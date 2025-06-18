cdc0c48f8b9d5509167c36c41e4eb0ad151e972a
Evertontorteli <evertontorteli@gmail.com>
2025-06-18 16:54:01 -0300

    index url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import axios from 'axios';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 
 const root = createRoot(document.getElementById("root"));
